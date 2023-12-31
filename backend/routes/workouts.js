c2b173291f426d83cdd8ebbfc768ebe9b26df75d
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2023-12-31 12:38:02 +0500

    feat[routes]: Implemented workout API endpoints

---
+++ backend/routes/workouts.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router()
+
+
+// GET / - Get all workouts
+router.get('/', (req, res) => {
+    res.json({ message: 'Get all workouts' })
+})
+
+
+// GET /:id - Get a single workout
+router.get('/:id', (req, res) => {
+    res.json({ message: 'Get a single workout' })
+})
+
+
+// POST / - Create a workout
+router.post('/', (req, res) => {
+    res.json({ message: 'Create a workout' })
+})
+
+
+// DELETE /:id - Delete a workout
+router.delete('/:id', (req, res) => {
+    res.json({ message: 'Delete a workout' })
+})
+
+
+// PATCH /:id - Update a workout
+router.patch('/:id', (req, res) => {
+    res.json({ message: 'Update a workout' })
+})
+
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT
+const workoutRoutes = require('./routes/workouts')
+
+
+app.use(express.json())
+app.use('/api/workouts', workoutRoutes)
 
 
 app.get('/', (req, res) => {
