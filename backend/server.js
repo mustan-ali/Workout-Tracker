0858386612c85b08d55df50db2b38ef6521b3c97
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2023-12-31 13:11:46 +0500

    feat(database): Set up connection to database

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 
@@ -9,11 +10,10 @@ app.use(express.json())
 app.use('/api/workouts', workoutRoutes)
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-})
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Database Connected & Server running on port ${PORT}`)
+        })
+    })
+    .catch((err) => { console.log(err) })
