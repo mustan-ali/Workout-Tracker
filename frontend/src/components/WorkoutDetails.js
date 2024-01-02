592ee04489f3f8da47979e98105ee753b30359e1
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-01-02 16:13:24 +0500

    feat(frontend): Added workout deletion feature

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,11 +1,16 @@
 const WorkoutDetails = ({ workout }) => {
 
+    const handleClick = async () => {
+        await fetch('http://localhost:4000/api/workouts/' + workout._id, { method: 'DELETE' })
+    }
+
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <p><strong>Weight (kg): </strong>{workout.weight}</p>
             <p><strong>Number of reps: </strong>{workout.reps}</p>
             <p><strong>Date Created: </strong>{workout.createdAt}</p>
+            <span onClick={handleClick}>Delete</span>
         </div>
     )
 }
